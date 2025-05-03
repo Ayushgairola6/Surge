0a4fd5329d7bf4a4207c35fe793047169d5adcf0
ayush <ayushgairola2002@gmail.com>
2025-05-03 14:05:15 +0530

    fixed the window object error

--- frontend/src/app/components/backgroundEffect.jsx
+++ frontend/src/app/components/backgroundEffect.jsx
@@ -2,23 +2,28 @@ import { motion } from 'framer-motion';
 import React, { useMemo } from 'react'
 
 const BackgroundEffect = () => {
-    const emojis = [
-        '💔', '🤫', '📝', '😈', '🔥', '😢', '👻', '🗣️', '🎭', '❤️',
-      ];
-      const createParticles = (count) => {
-        return Array.from({ length: count }).map((_, i) => {
-          const size = 20 + Math.random() * 30;
-          const x = Math.random() * window.innerWidth;
-          const y = Math.random() * window.innerHeight;
-          const delay = Math.random() * 5;
-          const duration = 5 + Math.random() * 5;
-          const emoji = emojis[Math.floor(Math.random() * emojis.length)];
-          return { id: i, size, x, y, delay, duration, emoji };
-        });
-      };
-      const particles = useMemo(() => createParticles(20), []);
-    return (<>
-        <div className="absolute inset-0 overflow-hidden pointer-events-none z-0 h-full w-full">
+  const emojis = [
+    '💔', '🤫', '📝', '😈', '🔥', '😢', '👻', '🗣️', '🎭', '❤️',
+  ];
+  const createParticles = (count) => {
+    return Array.from({ length: count }).map((_, i) => {
+      const size = 20 + Math.random() * 30;
+      const x = Math.random() * window.innerWidth;
+      const y = Math.random() * window.innerHeight;
+      const delay = Math.random() * 5;
+      const duration = 5 + Math.random() * 5;
+      const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+      return { id: i, size, x, y, delay, duration, emoji };
+    });
+  };
+  const particles = useMemo(() => {
+    if (typeof window !== "undefined") {
+      createParticles(20)
+    }
+  }, []);
+
+  return (<>
+    <div className="absolute inset-0 overflow-hidden pointer-events-none z-0 h-full w-full">
       {particles.map(({ id, size, x, y, delay, duration, emoji }) => (
         <motion.span
           key={id}
@@ -42,6 +47,6 @@ const BackgroundEffect = () => {
         </motion.span>
       ))}
     </div>
-    </>)
+  </>)
 }
 export default BackgroundEffect;
